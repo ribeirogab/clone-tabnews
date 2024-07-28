dbc8ba090503c54ec31092dc84be9c912661cf1d
ribeirogab <ribeirogabx@gmail.com>
2024-07-28 17:53:01 -0300

    chore: update cn util and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,16 @@ if (process.env.NODE_ENV === 'development') {
   const os = await import('os');
   const networkInterfaces = os.networkInterfaces();
 
-  if (networkInterfaces?.wlp8s0 && networkInterfaces.wlp8s0[0]?.address) {
-    const ip = networkInterfaces.wlp8s0[0].address;
-    const url = `http://${ip}:3000`;
+  const interfaces = ['wlp8s0', 'en0', 'en1'];
 
-    console.log('  - Local URL: ', url);
+  for (const iface of interfaces) {
+    if (networkInterfaces[iface]) {
+      for (const details of networkInterfaces[iface]) {
+        if (details.family === 'IPv4' && !details.internal) {
+          console.log(`  - Network IP: http://${details.address}:3000`);
+        }
+      }
+    }
   }
 }
 
